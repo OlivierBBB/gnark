2a4c037a0f72b8a4dc5060c045c7bc8bd87eaf6e
= <olivier.begassat.cours@gmail.com>
2020-12-02 20:28:16 +0100

    micro changes in PrefoldedCopyAndCipherLinComb

--- examples/gkr/copy_cipher.go
+++ examples/gkr/copy_cipher.go
@@ -34,40 +34,34 @@ func PrefoldedCopyAndCipher(cs *frontend.ConstraintSystem, Q frontend.Variable) 
 // PrefoldedCopyAndCipherLinComb generates two linear combinations of prefolded gate BKTs
 //		prefoldedCopy(?,?) := lambda * Copy(hL,?,?) + rho * Copy(hR,?,?)
 //		prefoldedCipher(?,?) := lambda * Cipher(hL,?,?) + rho * Cipher(hR,?,?)
-// these BKTs contain a nonzero value only for (?,?) == (0,1) i.e. in BKT.Table[1]
+// these BKTs contain a nonzero value only for (?,?) == (1, 0) i.e. in (BKT_name).Table[2]
 func PrefoldedCopyAndCipherLinComb(cs *frontend.ConstraintSystem, lambda, rho, hL, hR frontend.Variable) (prefoldedCopy, prefoldedCipher PrefoldedGateBKT) {
 
-	// populate prefoldedCopy
-	// copy(a,b,c) = a * (1-b) * c
-	// prefoldedCopy.Table[01] = lambda * hL + rho * hR
+	// prefoldedCopy and prefoldedCipher contain a single nonzero entry at position 2.
 
-	// cs.Println("hL:", hL)
-	// cs.Println("hR:", hR)
-	aux1 := cs.Mul(lambda, hL)                  // lambda * hL
-	aux2 := cs.Mul(rho, hR)                     // rho * hR
-	prefoldedCopy.Table[2] = cs.Add(aux1, aux2) // lambda * hL + rho * hR
-	// force zeros everywhere else --- is this necessary ?
+	// copy(a,b,c) = a * (1-b) * c
+	// prefoldedCopy.Table[2] = hL + rho * hR
+	aux := cs.Mul(rho, hR) // rho * hR
+	aux = cs.Add(hL, aux)  // hL + rho * hR
+	prefoldedCopy.Table[2] = aux
+	// zeros everywhere else
 	for i := 0; i < len(prefoldedCopy.Table); i++ {
 		if i != 2 {
 			prefoldedCopy.Table[i] = cs.Constant(0)
 		}
 	}
 
-	// populate prefoldedCipher
 	// cipher(a,b,c) = (1-a) * (1-b) * c
-	// prefoldedCipher.Table[01] = lambda * (1 - hL) + rho * (1 - hR)
-	aux1 = cs.Sub(lambda, aux1) // lambda - lambda * hL
-	aux2 = cs.Sub(rho, aux2)    // rho - rho * hR
-	prefoldedCipher.Table[2] = cs.Add(aux1, aux2)
-	// force zeros everywhere else --- is this necessary ?
+	// prefoldedCipher.Table[2] = (1 - hL) + rho * (1 - hR) = (1 + rho) - (hL + rho * hR)
+	aux = cs.Sub(cs.Constant(1), aux) // 1 - (hL + rho * hR)
+	aux = cs.Add(aux, rho)            // (1 + rho) - (hL + rho * hR)
+	prefoldedCipher.Table[2] = aux
+	// zeros everywhere else
 	for i := 0; i < len(prefoldedCipher.Table); i++ {
 		if i != 2 {
 			prefoldedCipher.Table[i] = cs.Constant(0)
 		}
 	}
-
-	// cs.Println("prefoldedCopy[2]:", prefoldedCopy.Table[2])
-	// cs.Println("prefoldedCipher[2]:", prefoldedCipher.Table[2])
 
 	return prefoldedCopy, prefoldedCipher
 
